d9f32917fdd8eca81e5184b7ac4576fec428eae5
Subhasree Bose <subhasree.bose2014@gmail.com>
2020-05-21 16:03:20 +0530

    second

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+});
+function submitted()
+{
+    var x=document.getElementById("text").value;
+    if(x.length>0)
+    {
+    alert("Thank you for your time!");
+    }
+    else{
+        alert("May be next time!");
+    }
+}
