0342d7d92e49155acc9d73d469cffba1d4a4f8c0
Subhasree Bose <subhasree.bose2014@gmail.com>
2020-05-22 13:26:56 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-import * from "animate.css";
 
-$(document).ready(function(){
-    $("body").show(2000);
-});
 function submitted()
 {
     var x=document.getElementById("text").value;
     if(x.length>0)
     {
+        document.getElementById("text").value="";
     alert("Thank you for your time!");
     }
     else{
